d9fd3ad617aa8749106d887d78e8b47fd226a95a
Victor (Vigge) B. <vigge15@proton.me>
2024-04-26 11:59:35 +0200

    Nearly done with header, dropdown and drawer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,103 +1,277 @@
-import * as MUI from '@mui/material/';
-import * as MUIIcons from '@mui/icons-material';
-import { useState, useEffect, useRef } from "react";
+import * as MUI from "@mui/material/";
+import * as MUIIcons from "@mui/icons-material";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "../styles/header.css";
-import hillringsberg from "../images/main/logotype.png";
+import hillringsbergIFLogo from "../images/main/logotype.png";
 
 export default function Header() {
   const navigate = useNavigate();
-
-  const [isOpenMenuOverlay, setIsOpenMenuOverlay] = useState(false);
-  const [isPersonIconOpen, setIsPersonIconOpen] = useState("");
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
 
   // Dropdown
   const [headerDropdown, setHeaderDropdown] = useState(false);
   const [dropdownAnchorEl, setDropdownAnchorEl] = useState(null);
 
-  //   ser till att menuOverlayen inte är öppen när skärmen är över 701px
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-      if (window.innerWidth > 701) {
-        setIsOpenMenuOverlay(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  // öppna och stäng menyoverlayen i mobilmode
-  const toggleMenu = () => {
-    setIsOpenMenuOverlay(!isOpenMenuOverlay);
-  };
-
-  // Navigerar till och från "/" från person ikonen i navbaren
-  const handlePersonIconClick = () => {
-    if (!isPersonIconOpen) {
-      setIsPersonIconOpen(true);
-    } else {
-      setIsPersonIconOpen(false);
-    }
-  };
+  // Drawer
+  const [headerDrawer, setHeaderDrawer] = useState(false);
 
   return (
-    <>
-      <section className="header">
-        <MUI.Link to="/start">
-          <img
-            className="header__container--loggo"
-            src={hillringsberg}
-            alt="Hillringsbergslogga"
-          />
-        </MUI.Link>
-        <div className="header__container" >
-          <MUI.Button id="header__dropdown-button" aria-controls={headerDropdown ? 'header__dropdown-menu' : undefined} aria-expanded={headerDropdown ? 'true' : undefined} variant="outlined" type="button" aria-haspopup="true" onClick={(event) => {setHeaderDropdown(!headerDropdown); setDropdownAnchorEl(event.currentTarget)}} disableElevation endIcon={headerDropdown ? <MUIIcons.KeyboardArrowUp /> : <MUIIcons.KeyboardArrowDown />} sx={{backgroundColor: 'transparent', color: '#fff', border: '1px solid transparent'}}>
-            Om Eljusspåret
-          </MUI.Button>
-            <MUI.Menu id="header__dropdown-menu" MenuListProps={{'aria-labelledby': 'header__dropdown-button'}} open={headerDropdown} onClose={() => setHeaderDropdown(false)} anchorEl={dropdownAnchorEl}>
-              <MUI.MenuItem disableRipple onClick={() => navigate("/projektet-naturkraft")}>Projektet Naturkraft</MUI.MenuItem>
-              <MUI.MenuItem disableRipple onClick={() => navigate("/samarbetspartners")}>Samarbetspartners</MUI.MenuItem>
-              <MUI.MenuItem disableRipple onClick={() => navigate("/vill-du-veta-mer")}>Vill du veta mer?</MUI.MenuItem>
-            </MUI.Menu>
-
-          <MUI.Link to="/kontakt" className="header__container--navbar">
-            Hitta hit
-          </MUI.Link>
-
-          <MUI.Link
-            to={isPersonIconOpen ? "/" : "/login"}
-            className="header__container--navbar material-symbols-outlined person"
-            onClick={handlePersonIconClick}
+    <MUI.AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#111",
+        height: "80px",
+        justifyContent: "center",
+        width: "100vw",
+      }}
+    >
+      <MUI.Container
+        maxWidth="xl"
+        sx={{
+          paddingLeft: { xs: "0.75em", md: "1em", lg: "1.25em", xl: "1.5em" },
+          paddingRight: { xs: "0.75em", md: "1em", lg: "1.25em", xl: "1.5em" },
+        }}
+      >
+        <MUI.Toolbar
+          disableGutters
+          sx={{ flexDirection: "row", justifyItems: "center" }}
+        >
+          <MUI.IconButton
+            size="small"
+            title="Navigera till startsidan"
+            onClick={() => {
+              navigate("/start");
+              setHeaderDrawer(false);
+            }}
           >
-            person
-          </MUI.Link>
-        </div>
-
-        {/* Mobilikoner */}
-        <span className="material-symbols-outlined menu" onClick={toggleMenu}>
-          Menu
-        </span>
-      </section>
-      {isOpenMenuOverlay && windowWidth <= 701 && (
-        <div className="menu-overlay">
-          <div className="menu-overlay__content">
-          
-
-            <MUI.Link className="menu-item" to="/projektet-naturkraft">Projektet Naturkraft</MUI.Link>
-            <MUI.Link className="menu-item" to="/samarbetspartners">Samarbetspartners</MUI.Link>
-            <MUI.Link className="menu-item" to="/vill-du-veta-mer">Vill du veta mer?</MUI.Link>
-            <MUI.Link className="menu-item" to="/vill-du-veta-mer">Hitta hit</MUI.Link>
-          </div>
-        </div>
-      )}
-    </>
+            <img
+              src={hillringsbergIFLogo}
+              alt="Hillringsbergslogga"
+              width="65px"
+              height="65px"
+            />
+          </MUI.IconButton>
+          <MUI.Box
+            justifyContent="flex-end"
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            <MUI.Button
+              id="header__dropdown-button"
+              aria-controls={
+                headerDropdown ? "header__dropdown-menu" : undefined
+              }
+              aria-expanded={headerDropdown ? "true" : undefined}
+              variant="outlined"
+              type="button"
+              aria-haspopup="true"
+              onClick={(event) => {
+                setHeaderDropdown(!headerDropdown);
+                setDropdownAnchorEl(event.currentTarget);
+              }}
+              disableElevation
+              endIcon={
+                headerDropdown ? (
+                  <MUIIcons.KeyboardArrowUp />
+                ) : (
+                  <MUIIcons.KeyboardArrowDown />
+                )
+              }
+              sx={{
+                backgroundColor: "transparent",
+                color: "#fff",
+                border: "1px solid transparent",
+              }}
+            >
+              Om Eljusspåret
+            </MUI.Button>
+            <MUI.Menu
+              id="header__dropdown-menu"
+              MenuListProps={{ "aria-labelledby": "header__dropdown-button" }}
+              open={headerDropdown}
+              onClose={() => setHeaderDropdown(false)}
+              anchorEl={dropdownAnchorEl}
+            >
+              <MUI.MenuItem
+                disableRipple
+                onClick={() => {
+                  navigate("/projektet-naturkraft");
+                  setHeaderDropdown(false);
+                }}
+              >
+                Projektet Naturkraft
+              </MUI.MenuItem>
+              <MUI.MenuItem
+                disableRipple
+                onClick={() => {
+                  navigate("/samarbetspartners");
+                  setHeaderDropdown(false);
+                }}
+              >
+                Samarbetspartners
+              </MUI.MenuItem>
+              <MUI.MenuItem
+                disableRipple
+                onClick={() => {
+                  navigate("/vill-du-veta-mer");
+                  setHeaderDropdown(false);
+                }}
+              >
+                Vill du veta mer?
+              </MUI.MenuItem>
+            </MUI.Menu>
+            <MUI.Button
+              disableElevation
+              variant="outlined"
+              type="button"
+              onClick={() => navigate("/kontakt")}
+              sx={{
+                backgroundColor: "transparent",
+                color: "#fff",
+                border: "1px solid transparent",
+              }}
+            >
+              Hitta hit
+            </MUI.Button>
+          </MUI.Box>
+          <MUI.Box
+            justifyContent="flex-end"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            <MUI.IconButton
+              id="header__drawer-button"
+              aria-controls={headerDrawer ? "header__drawer" : undefined}
+              aria-expanded={headerDrawer ? "true" : undefined}
+              variant="outlined"
+              type="button"
+              role="button"
+              aria-haspopup="true"
+              onClick={() => setHeaderDrawer(!headerDrawer)}
+              sx={{
+                backgroundColor: "transparent",
+                color: "#fff",
+                border: "1px solid transparent",
+              }}
+            >
+              {headerDrawer ? <MUIIcons.Close /> : <MUIIcons.Menu />}
+            </MUI.IconButton>
+            <MUI.Drawer
+              anchor="right"
+              id="header__menu"
+              open={headerDrawer}
+              onClose={() => setHeaderDrawer(false)}
+              hideBackdrop
+              variant="temporary"
+              sx={{
+                top: "80px",
+                display: { xs: "block", md: "none" },
+                "& .MuiDrawer-paper": {
+                  top: "80px",
+                  width: "100%",
+                  backgroundColor: "#111",
+                  color: "#fff",
+                },
+              }}
+            >
+              <MUI.Box component="section" role="presentation">
+                <MUI.List>
+                  <MUI.Accordion
+                    defaultExpanded
+                    sx={{ backgroundColor: "#111", color: "#fff" }}
+                  >
+                    <MUI.AccordionSummary
+                      sx={{
+                        "& .MuiAccordionSummary-content": {
+                          display: "grid",
+                          placeItems: "center",
+                          textAlign: "center",
+                        },
+                      }}
+                      expandIcon={
+                        <MUIIcons.ExpandMore sx={{ color: "#fff" }} />
+                      }
+                    >
+                      <MUI.Typography>Om Eljusspåret</MUI.Typography>
+                    </MUI.AccordionSummary>
+                    <MUI.AccordionDetails>
+                      <MUI.List>
+                        <MUI.ListItem disablePadding>
+                          <MUI.ListItemButton
+                            sx={{
+                              "& .MuiListItemText-root": {
+                                display: "grid",
+                                placeItems: "center",
+                                textAlign: "center",
+                              },
+                            }}
+                            onClick={() => {
+                              navigate("/projektet-naturkraft");
+                              setHeaderDrawer(!headerDrawer);
+                            }}
+                          >
+                            <MUI.ListItemText primary="Projektet Naturkraft" />
+                          </MUI.ListItemButton>
+                        </MUI.ListItem>
+                        <MUI.ListItem disablePadding>
+                          <MUI.ListItemButton
+                            sx={{
+                              "& .MuiListItemText-root": {
+                                display: "grid",
+                                placeItems: "center",
+                                textAlign: "center",
+                              },
+                            }}
+                            onClick={() => {
+                              navigate("/samarbetspartners");
+                              setHeaderDrawer(!headerDrawer);
+                            }}
+                          >
+                            <MUI.ListItemText primary="Samarbetspartners" />
+                          </MUI.ListItemButton>
+                        </MUI.ListItem>
+                        <MUI.ListItem disablePadding>
+                          <MUI.ListItemButton
+                            sx={{
+                              "& .MuiListItemText-root": {
+                                display: "grid",
+                                placeItems: "center",
+                                textAlign: "center",
+                              },
+                            }}
+                            onClick={() => {
+                              navigate("/vill-du-veta-mer");
+                              setHeaderDrawer(!headerDrawer);
+                            }}
+                          >
+                            <MUI.ListItemText primary="Vill du veta mer?" />
+                          </MUI.ListItemButton>
+                        </MUI.ListItem>
+                      </MUI.List>
+                    </MUI.AccordionDetails>
+                  </MUI.Accordion>
+                  <MUI.ListItem disablePadding>
+                    <MUI.ListItemButton
+                      sx={{
+                        "& .MuiListItemText-root": {
+                          display: "grid",
+                          placeItems: "center",
+                          textAlign: "center",
+                        },
+                      }}
+                      onClick={() => {
+                        navigate("/kontakt");
+                        setHeaderDrawer(!headerDrawer);
+                      }}
+                    >
+                      <MUI.ListItemText primary="Hitta hit" />
+                    </MUI.ListItemButton>
+                  </MUI.ListItem>
+                </MUI.List>
+              </MUI.Box>
+            </MUI.Drawer>
+          </MUI.Box>
+        </MUI.Toolbar>
+      </MUI.Container>
+    </MUI.AppBar>
   );
 }
