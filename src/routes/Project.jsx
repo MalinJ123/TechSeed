1a3218b5b64d760f7c88876a147ada383be12f08
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-04-30 10:10:16 +0200

    fixed the size for text with responsive design for small screen to large screen

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -3,25 +3,35 @@ import "../styles/project.css";
 
 function Project() {
   return (
-    <MUI.Container sx={{ marginTop: 5, padding: {xs: 0} }}>
+    <MUI.Container sx={{ marginTop: 10, padding: {xs: 0} }}>
       <MUI.Box 
         display="flex" 
-        flexDirection="column" alignItems="center"
-        padding={5}>
-        <MUI.Card elevation={2} className="card"
+        flexDirection="column" alignItems="center">
+        <MUI.Card elevation={2} 
           sx={{
             backgroundColor: "rgba(255, 255, 255, 0.95)",
+            padding: 2, 
           }}
         >
-          <MUI.CardHeader className="text-card" title="Elljusspåret i Glava" 
-          titleTypographyProps={{ textAlign: 'center', fontSize: {sm: 30, md: 36}, marginBottom: 5}} subheader="Glava elljusspår har något så speciellt som ett smart, behovsdrivet
+          <MUI.CardHeader className="text-card" title="Elljusspåret i Glava"
+          titleTypographyProps={{ textAlign: 'center', fontSize: {xs: 32, md: 36}, marginBottom: 3, marginTop: 2}} subheader="Glava elljusspår har något så speciellt som ett smart, behovsdrivet
               och modulärt energisystem baserat i solenergi. Kombinationen av
               solenergi, behovsstyrd energieffektiv belysning och lagring via
               nätet möjliggör en miljövänlig energiförsörjning. Samtidigt är
-              utomhusmiljön både trygg och trivsam." subheaderTypographyProps={{fontWeight: 'bolder', fontSize: {sm: 18, md: 24}}} />
+              utomhusmiljön både trygg och trivsam." subheaderTypographyProps={{fontWeight: 'bolder', fontSize: {xs: 18, md: 26}, paddingX: {xs: 2, md: 3}}} />
           <MUI.CardContent>
             <MUI.Typography
-            variant="body2" component="p" gutterBottom fontSize={20} lineHeight={2}>
+            variant="body2" component="p" gutterBottom lineHeight={2}
+            sx={{
+              paddingX: {
+                xs: 2, 
+                md: 3,
+              },
+              fontSize: {
+                xs: 18, 
+                md: 22,
+              }
+            }}>
               Anläggningen med solpark, batteri och LED-belysning finns
               installerad vid motionsspåret i Glava, nära Glava kyrka. Systemet
               togs fram åren 2018–2024 i projektet Naturkraft – Må gott på
@@ -31,7 +41,7 @@ function Project() {
               Glava Gymnastikförening.
             </MUI.Typography>
           </MUI.CardContent>
-          <MUI.CardActions>
+          <MUI.CardActions sx={{marginLeft: 3}}>
             <MUI.Link
               underline="hover"
               component="button"
