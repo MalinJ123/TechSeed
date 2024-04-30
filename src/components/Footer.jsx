eae784442a238d0ef16f40e9bb9508d97d683eb9
MalinJ123 <malin--jonsson@hotmail.com>
2024-04-30 15:30:51 +0200

    footer need jura font family

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,72 @@
 import * as MUI from "@mui/material";
-import CompanyLogo from '../images/main/logotype.png';
-
+import CompanyLogo from "../images/main/logotype.png";
 function Footer() {
   return (
-    <>
-      <MUI.Container
-        sx={{ marginTop: 4, backgroundColor: "black", color: "white", padding: 2 }}
+    <MUI.Container
+      sx={{
+       marginTop: 4,
+        backgroundColor: "black",
+        color: "white",
+        padding: 2,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginLeft: "1em",  
+        //   border:"1px solid hotpink"
+        }}
       >
-        <MUI.Grid container justifyContent="space-between">
-          <MUI.Grid item xs={12} sm={4}>
-            <MUI.Typography variant="body1">
-              Hillringsberg 671 97 Glava
+        <div style={{ flex: 1, padding: "8px",    alignContent:"center"}}>
+          <MUI.Typography variant="body1" fontSize="18px">
+            Hillringsberg 671 97 Glava
+          </MUI.Typography>
+
+          <MUI.Typography variant="body2" fontSize="16px">Bengt@delabglava.se</MUI.Typography>
+
+          <MUI.Typography variant="body3" fontSize="18px"> 070 - 34 26 345</MUI.Typography>
+        </div>
+
+        <div
+          style={{
+            // border: "1px solid hotpink",
+            flex: 1,
+            padding: "8px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <MUI.Avatar
+            alt="Hillringsbergs logga"
+            src={CompanyLogo}
+            sx={{ width: 100, height: 100 }}
+          />
+        </div>
+
+        <div
+          style={{
+            //  border: "1px solid hotpink",
+            flex: 1,
+            marginRight: "1em",
+            alignContent:"center"
+          }}
+        >
+          <div style={{ textAlign: "center" }}>
+            <MUI.Typography
+              variant="body2" fontSize="18px"
+              style={{ textAlign: "right", marginTop: "1em" }}
+            >
+              Isac.Myren.Andersson@GlavaEnergyCenter.se
             </MUI.Typography>
-            <MUI.Typography variant="body1">
-              bengt@hotmail.com
+            <MUI.Typography variant="body2" fontSize="18px" style={{ textAlign: "right" }}>
+              Scaaler IoT Labs, Strandvägen 2, 671 51 Arvika
             </MUI.Typography>
-            <MUI.Typography variant="body1">
-              070-234 234
-            </MUI.Typography>
-          </MUI.Grid>
-          <MUI.Grid item xs={12} sm={4} textAlign="center">
-          <MUI.Avatar alt="Company Logo" src={CompanyLogo} sx={{ width: 100, height: 100 }} />
-     
-          
-          </MUI.Grid>
-          <MUI.Grid item xs={12} sm={4}>
-            <MUI.Typography variant="body1" textAlign="right">
-              Hitta hit
-            </MUI.Typography>
-            <MUI.Typography variant="body1" textAlign="right">
-              isac.myren.andersson@hotmail.com
-            </MUI.Typography>
-            <MUI.Typography variant="body1" textAlign="right">
-              Scaaler IOT. adress
-            </MUI.Typography>
-          </MUI.Grid>
-        </MUI.Grid>
-      </MUI.Container>
-    </>
+          </div>
+        </div>
+      </div>
+    </MUI.Container>
   );
 }
 
