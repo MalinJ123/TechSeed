2fc9655b54ff6632da8a1a513a40e8cb46fe94e1
Victor (Vigge) B. <vigge15@proton.me>
2024-05-02 11:34:35 +0200

    Removed app

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import App from './App.jsx'
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { Router } from './RouteConfig'
-import { ContextRoot } from './ContextRoot'
-import { RouterProvider } from 'react-router-dom';
+import { Router } from "./RouteConfig";
+import { ContextRoot } from "./ContextRoot";
+import { RouterProvider } from "react-router-dom";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextRoot>
       <RouterProvider router={Router} />
     </ContextRoot>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
