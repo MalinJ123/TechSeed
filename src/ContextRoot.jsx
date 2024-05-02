1a4b0e0dc62a9d4d25ad9a861b5863944bf3c525
Victor (Vigge) B. <vigge15@proton.me>
2024-05-02 11:52:28 +0200

    Update

--- src/ContextRoot.jsx
+++ src/ContextRoot.jsx
@@ -1,15 +1,6 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const ContextRoot = ({ children }) => {
-  const [showTextContainer, setShowTextContainer] = useState(true);
-
-  const handleNavigation = () => {
-    setShowTextContainer(false);
-  };
-  return (
-    <AppContext.Provider value={{ showTextContainer, handleNavigation }}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
